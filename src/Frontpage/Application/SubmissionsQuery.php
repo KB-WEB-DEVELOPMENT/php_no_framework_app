d6d7fddc4f27a611d15c4d1110c4a10ede924c49
Kami Barut-Wanayo <kamibarut@yahoo.com>
2023-12-30 18:58:17 +0100

    Create SubmissionsQuery.php

---
+++ src/Frontpage/Application/SubmissionsQuery.php
@@ -0,0 +1,10 @@
+<?php 
+declare(strict_types=1);
+
+namespace SocialNews\FrontPage\Application;
+
+interface SubmissionsQuery
+{
+    /** @return Submission[] */
+    public function execute(): array;
+}
