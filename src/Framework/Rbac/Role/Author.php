0176265b20e91c220b415fbdf49913e42304247e
Kami Barut-Wanayo <kamibarut@yahoo.com>
2023-12-30 18:50:09 +0100

    Create Author.php

---
+++ src/Framework/Rbac/Role/Author.php
@@ -0,0 +1,15 @@
+<?php 
+declare(strict_types=1);
+
+namespace SocialNews\Framework\Rbac\Role;
+
+use SocialNews\Framework\Rbac\Permission\SubmitLink;
+use SocialNews\Framework\Rbac\Role;
+
+final class Author extends Role
+{
+    protected function getPermissions(): array
+    {
+        return [new SubmitLink()];
+    }
+}
