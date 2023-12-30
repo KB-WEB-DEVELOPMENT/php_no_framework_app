0af96eae2ce3b90395f9193817e4a0854b1ea324
Kami Barut-Wanayo <kamibarut@yahoo.com>
2023-12-30 19:04:10 +0100

    Create Submission.php

---
+++ src/Submission/Domain/Submission.php
@@ -0,0 +1,64 @@
+<?php 
+declare(strict_types=1);
+
+namespace SocialNews\Submission\Domain;
+
+use DateTimeImmutable;
+use Ramsey\Uuid\UuidInterface;
+use Ramsey\Uuid\Uuid;
+
+final class Submission
+{
+    private $id;
+    private $url;
+    private $title;
+    private $creationDate;
+
+    public function __construct(
+        UuidInterface $id,
+        string $url,
+        string $title,
+        DateTimeImmutable $creationDate
+    ) {
+        $this->id = $id;
+        $this->url = $url;
+        $this->title = $title;
+        $this->creationDate = $creationDate;
+    }
+
+    public function getId(): UuidInterface
+    {
+        return $this->id;
+    }
+
+    public function getAuthorId(): AuthorId
+    {
+        return $this->authorId;
+    }
+
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function getCreationDate(): DateTimeImmutable
+    {
+        return $this->creationDate;
+    }
+
+    public static function submit(UuidInterface $authorId,string $url,string $title): Submission 
+    {
+        return new Submission(
+            Uuid::uuid4(),
+            AuthorId::fromUuid($authorId),
+            $url,
+            $title,
+            new DateTimeImmutable()
+        );
+    }
+}
